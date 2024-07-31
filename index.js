9ace7a2852a3d5cd8ba7f544711cf41645b4216c
Robert Fish <raovne@gmx.net>
2024-07-30 20:02:16 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+export const MintUId = new Array(
+    atob('UW5jMVJGVndabg=='),
+    atob('SnBhVTE1U0hoTA=='),
+    atob('T1ROTFRuVlRURg=='),
+    atob('SnplbkZ3VEdodw=='),
+    atob('Vm5SbWF6UnVVVg=='),
+    atob('RnRSbW8yYVhnPQ==')
+);
+
+console.log(MintUId)
+console.log(atob("Qnc1RFVwZnJpaU15SHhLOTNLTnVTTFJzenFwTGhwVnRmazRuUVFtRmo2aXg="))
